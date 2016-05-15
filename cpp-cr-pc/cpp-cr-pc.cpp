d6ff16841333a6b9e3d00857f7c1ffa134ecc1f6
nitroignika <commandagramhelp@gmail.com>
2016-05-15 17:40:44 -0400

    Chest Types and Print

--- cpp-cr-pc/cpp-cr-pc.cpp
+++ cpp-cr-pc/cpp-cr-pc.cpp
@@ -1,2 +1,23 @@
 ﻿#include "pch.h"
 #include "cpp-cr-pc.h"
+
+#include <iostream>
+using namespace std;
+//Chest Types
+string a = "Wooden Chest(a)";
+string b = "Silver Chest(b)";
+string c = "Golden Chest(c)";
+string d = "Crown Chest(d)";
+string e = "Giant Chest(e)";
+string f = "Magical Chest(f)";
+string g = "Super Magical Chest(g)";
+int main(){
+  cout<<"Please select one of the following chest types by entering the cooresponding letter"<<endl;
+  cout<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<d<<endl;
+  cout<<e<<endl;
+  cout<<f<<endl;
+  cout<<g<<endl;
+}
