5b3d4c4523f5bb662c83cd09d27030e8fe1285c9
nitroignika <commandagramhelp@gmail.com>
2016-05-15 18:08:07 -0400

    Variables and Conditions
    Added all of the variables, and conditions for the chests. No calculations have been added yet

--- cpp-cr-pc/cpp-cr-pc.cpp
+++ cpp-cr-pc/cpp-cr-pc.cpp
@@ -11,6 +11,25 @@ string d = "Crown Chest(d)";
 string e = "Giant Chest(e)";
 string f = "Magical Chest(f)";
 string g = "Super Magical Chest(g)";
+//Arenas
+string h = "Goblin Stadium(a)";
+string i = "Bone Pit(b)";
+string j = "Barbarian Bowl(c)"
+string k = "P.E.K.K.A's Playhouse(d)";
+string l = "Spell Valley(e)";
+string m = "Builder's Workshop(f)";
+string n = "Royal Arena(g)";
+string o = "Legendary Arena(h)";
+//Rarity
+string p = "Common(a)";
+string q = "Rare(b)";
+string r = "Epic(c)";
+string s = "Legendary(d)";
+//User Inputs
+string z; //Chest
+string x; //Arena
+string y; //Rarity
+
 int main(){
   cout<<"Please select one of the following chest types by entering the cooresponding letter"<<endl;
   cout<<a<<endl;
@@ -20,4 +39,28 @@ int main(){
   cout<<e<<endl;
   cout<<f<<endl;
   cout<<g<<endl;
+  cin>>z;
+    if(z=="a"){
+        cout<<"You picked a Wooden Chest."<<endl;
+    }
+    if(z=="b"){
+        cout<<"You picked a Silver Chest."<<endl;
+    }
+    if(z=="c"){
+        cout<<"You picked a Golden Chest."<<endl;
+    }
+    if(z=="d"){
+        cout<<"You picked a Crown Chest."<<endl;
+    }
+    if(z=="e"){
+        cout<<"You picked a Giant Chest."<<endl;
+    }
+    if(z=="f"){
+        cout<<"You picked a Magical Chest."<<endl;
+    }
+    if(z=="g"){
+        cout<<"You picked a Super Magical Chest."<<endl;
+    }
+    return 0;
 }
+
